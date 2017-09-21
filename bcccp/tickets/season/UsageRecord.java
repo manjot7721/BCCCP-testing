d2afcf17aabb1a728aca9f7ce5e5d941880698d7
manjot7721 <30396993+manjot7721@users.noreply.github.com>
2017-09-21 20:25:49 +1000

    updation in endDateTime() method in usageRecord module
    if end time is not set it will return zero and if end time is set then it will return endDateTime

--- bcccp/tickets/season/UsageRecord.java
+++ bcccp/tickets/season/UsageRecord.java
@@ -43,7 +43,10 @@ public class UsageRecord implements IUsageRecord {
 
 	@Override
 	public long getEndTime() {
-		return endDateTime;
+		if(endDateTime == this.setEndTime)
+			return endDateTime;
+		else
+			return 0;
 	}
 
 
